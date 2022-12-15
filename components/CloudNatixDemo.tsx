01893e99fa7ca8efba4eb162645241d5b33c0e0f
Sulaiman Hamouda <sulaiman@cloudnatix.com>
2022-12-15 18:48:50 -0500

    update

---
+++ components/CloudNatixDemo.tsx
@@ -0,0 +1,12 @@
+export const CloudNatixDemo = () => {
+  return (
+    <iframe
+      title='vimeo-player'
+      src='https://player.vimeo.com/video/625625646?h=21e6ec9153'
+      width='1000'
+      height='800'
+      allowFullScreen
+      className='w-full'
+    ></iframe>
+  )
+}
