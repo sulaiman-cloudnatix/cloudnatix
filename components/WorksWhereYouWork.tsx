e722cc94f090489ea913c723eec57b4443bc396c
Sulaiman Hamouda <sulaiman@cloudnatix.com>
2022-12-15 18:04:25 -0500

    update

--- components/WorksWhereYouWork.tsx
+++ components/WorksWhereYouWork.tsx
@@ -6,7 +6,7 @@ export function WorksWhereYouWork() {
       <div className='mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8 flex justify-center'>
         <div className='text-center flex flex-col w-max'>
           <p className='mt-1 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl'>
-            Works where your work
+            Works where you work
           </p>
 
           <p className='mx-auto mt-5 max-w-xl text-lg text-gray-500'>
