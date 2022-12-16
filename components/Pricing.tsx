c87fb7b119f04b13de3ddffde93befbd08c345cc
Sulaiman Hamouda <sulaiman@cloudnatix.com>
2022-12-16 12:33:01 -0500

    update

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -73,47 +73,49 @@ export function Pricing() {
               {tiers.map((tier) => (
                 <div
                   key={tier.name}
-                  className='flex flex-col rounded-3xl bg-white shadow-xl ring-1 ring-black/10'
+                  className='shadow-xl w-full h-max rounded-3xl bg-gradient-to-r from-indigo-400 via-sky-400 to-sky-400 p-0.5'
                 >
-                  <div className='p-8 sm:p-10'>
-                    <h3
-                      className='text-lg font-semibold leading-8 tracking-tight text-natix'
-                      id={tier.id}
-                    >
-                      {tier.name}
-                    </h3>
-                    <div className='mt-4 flex items-baseline text-5xl font-bold tracking-tight text-gray-900'>
-                      {tier.priceMonthly}
+                  <div className='flex flex-col rounded-3xl bg-white'>
+                    <div className='p-8 sm:p-10'>
+                      <h3
+                        className='text-lg font-semibold leading-8 tracking-tight text-natix'
+                        id={tier.id}
+                      >
+                        {tier.name}
+                      </h3>
+                      <div className='mt-4 flex items-baseline text-5xl font-bold tracking-tight text-gray-900'>
+                        {tier.priceMonthly}
+                      </div>
+                      <p className='mt-6 text-base leading-7 text-gray-600'>
+                        {tier.description}
+                      </p>
                     </div>
-                    <p className='mt-6 text-base leading-7 text-gray-600'>
-                      {tier.description}
-                    </p>
-                  </div>
-                  <div className='flex flex-1 flex-col p-2'>
-                    <div className='flex flex-1 flex-col justify-between rounded-2xl bg-gray-50 p-6 sm:p-8'>
-                      <ul role='list' className='space-y-6'>
-                        {tier.features.map((feature) => (
-                          <li key={feature} className='flex items-start'>
-                            <div className='flex-shrink-0'>
-                              <CheckIcon
-                                className='h-6 w-6 text-natix'
-                                aria-hidden='true'
-                              />
-                            </div>
-                            <p className='ml-3 text-sm leading-6 text-gray-600'>
-                              {feature}
-                            </p>
-                          </li>
-                        ))}
-                      </ul>
-                      <div className='mt-8'>
-                        <a
-                          href={tier.href}
-                          className='inline-block w-full rounded-lg bg-natix px-4 py-2.5 text-center text-sm font-semibold leading-5 text-white shadow-md hover:bg-natix'
-                          aria-describedby={tier.id}
-                        >
-                          {tier.buttonTitle}
-                        </a>
+                    <div className='flex flex-1 flex-col p-2'>
+                      <div className='flex flex-1 flex-col justify-between rounded-2xl bg-gray-50 p-6 sm:p-8'>
+                        <ul role='list' className='space-y-6'>
+                          {tier.features.map((feature) => (
+                            <li key={feature} className='flex items-start'>
+                              <div className='flex-shrink-0'>
+                                <CheckIcon
+                                  className='h-6 w-6 text-natix'
+                                  aria-hidden='true'
+                                />
+                              </div>
+                              <p className='ml-3 text-sm leading-6 text-gray-600'>
+                                {feature}
+                              </p>
+                            </li>
+                          ))}
+                        </ul>
+                        <div className='mt-8'>
+                          <a
+                            href={tier.href}
+                            className='inline-block w-full rounded-lg bg-natix px-4 py-2.5 text-center text-sm font-semibold leading-5 text-white shadow-md hover:bg-natix'
+                            aria-describedby={tier.id}
+                          >
+                            {tier.buttonTitle}
+                          </a>
+                        </div>
                       </div>
                     </div>
                   </div>
