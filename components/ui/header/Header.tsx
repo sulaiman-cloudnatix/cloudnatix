dc8588ae3ac0b1fdea1f72b324cf8f3da1eb73dc
Sulaiman Hamouda <sulaiman@cloudnatix.com>
2022-12-16 12:21:29 -0500

    update

--- components/ui/header/Header.tsx
+++ components/ui/header/Header.tsx
@@ -183,7 +183,7 @@ export function Header() {
                               <div key={item.name} className='flow-root w-full'>
                                 <button
                                   onClick={item.href}
-                                  className='-m-3 flex items-center w-full justify-center rounded-md p-3 text-base font-medium text-gray-900 hover:bg-gray-100'
+                                  className='flex items-center w-full justify-center rounded-md p-3 text-base font-medium text-gray-900 hover:bg-gray-100'
                                 >
                                   <item.icon
                                     className='h-6 w-6 flex-shrink-0 text-gray-400'
